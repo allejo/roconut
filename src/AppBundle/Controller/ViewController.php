554a9002a27f843395e219aa91c82c855e65cd58
Vladimir Jimenez <allejo@me.com>
2017-11-25 02:00:42 -0800

    Clean up View controller
    - Throw 404s
    - Add parameter for different formats

--- src/AppBundle/Controller/ViewController.php
+++ src/AppBundle/Controller/ViewController.php
@@ -2,12 +2,13 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Paste;
 use AppBundle\Service\AnsiHtmlTransformer;
 use AppBundle\Service\Crypto;
+use AppBundle\Service\MessageLogTransformer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/view")
@@ -15,18 +16,32 @@ use Symfony\Component\HttpFoundation\Request;
 class ViewController extends Controller
 {
     /**
-     * @Route("/{id}/{key}", name="show_message_log")
+     * @Route("/{id}/{key}/{format}", name="show_message_log", defaults={"format": "html"})
+     *
+     * @param int    $id  The ID of the Paste we're accessing
+     * @param string $key The decryption key necessary to read this Paste
+     *
+     * @return Response
      */
-    public function viewAction(Request $request, $id, $key)
+    public function viewAction(Request $request, $id, $key, $format)
     {
         $em = $this->getDoctrine()->getManager();
-
-        /** @var Paste $paste */
         $paste = $em->getRepository('AppBundle:Paste')->find($id);
+
+        if ($paste === null || $key !== $paste->getEncryptionKey()) {
+            throw $this->createNotFoundException('This paste does not exist');
+        }
+
         $message = Crypto::decrypt_v1($paste->getMessage(), $key);
 
         $ansiTransformer = new AnsiHtmlTransformer();
         $message = $ansiTransformer->convert($message);
+
+        $msgTransformer = new MessageLogTransformer($message);
+        $message = $msgTransformer
+            ->filterLog($paste->getFilter())
+            ->displayMessages()
+        ;
 
         return $this->render(':view:message-log.html.twig', [
             'paste' => $paste,
