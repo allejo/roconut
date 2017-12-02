cb3b936d1ab001c88816d3dc0d46269d26f2a46d
Vladimir Jimenez <allejo@me.com>
2017-12-02 13:52:47 -0800

    Add phpDoc for DownloadResponse to fix IDE warnings

--- src/AppBundle/Response/DownloadResponse.php
+++ src/AppBundle/Response/DownloadResponse.php
@@ -9,6 +9,14 @@ class DownloadResponse extends Response
     const HTML_TYPE = 'text/html';
     const TEXT_TYPE = 'text/plain';
 
+    /**
+     * DownloadResponse constructor.
+     * @param string $content
+     * @param int    $filename
+     * @param string $mimeType
+     * @param int    $status
+     * @param array  $headers
+     */
     public function __construct($content, $filename, $mimeType, $status = 200, array $headers = array())
     {
         parent::__construct($content, $status, $headers);
